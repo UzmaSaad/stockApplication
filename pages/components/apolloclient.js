14a5bab973e52598f840288f8c378ec13ecc3054
Uzma <uzma.majeed2020@gmail.com>
2021-03-02 06:58:56 +0500

    Structure and APIs included

---
+++ pages/components/apolloclient.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import DataTable from './datatable'
+
+class apolloclient extends Component {
+    constructor(props) {
+        super(props);
+        state : {
+            country:{'name','native','capital','emoji','currency'}
+        }
+
+    }
+   async componentDidMount() {
+        const client = new ApolloClient({
+          uri: 'https://countries.trevorblades.com/',
+          cache: new InMemoryCache()
+        });
+
+     await  client
+  .query({
+    query: gql`
+    {
+        countries {
+          name
+          code
+        }
+      }
+    `
+  })
+  .then(result => 
+    //console.log('Api Result',result)
+    this.setState({country : result})
+    );
+    }
+
+    render() {
+        console.log('This is current state of apolloclient to check', this.state);
+        return (
+            <div>
+                <DataTable props={this.state}></DataTable>
+            </div>
+        );
+    }
+}
+
+apolloclient.propTypes = {
+
+};
+
+export default apolloclient;
---
+++ pages/components/datatable.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+ 
+const datatable = (props) => {
+  // const {data }= props;
+ console.log("This is Check Data",props);
+   
+    return (
+        <div>
+            datatable
+        </div>
+    );
+}
+ 
+datatable.propTypes = {};
+ 
+export default datatable;
